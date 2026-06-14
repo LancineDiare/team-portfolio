b624e38e6c10bb302b55e509bae3a05655d2b459
Chikamov1 <lancine.diare1@gmail.com>
2026-06-15 04:43:56 +0800

    Project structure created

---
+++ src/utils/logger.js
@@ -0,0 +1,15 @@
+const isDev = import.meta.env.DEV;
+
+export const logger = {
+  info: (...args) => {
+    if (isDev) console.log("[INFO]", ...args);
+  },
+
+  warn: (...args) => {
+    if (isDev) console.warn("[WARN]", ...args);
+  },
+
+  error: (...args) => {
+    console.error("[ERROR]", ...args);
+  }
+};
