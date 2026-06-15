4b52ec8bf595eb64cc242ef886066203a3ed6fa6
Chikamov1 <lancine.diare1@gmail.com>
2026-06-15 12:37:12 +0800

    Add dedicated projects page styling

--- src/pages/CommonProjects/CommonProjects.jsx
+++ src/pages/CommonProjects/CommonProjects.jsx
@@ -5,8 +5,11 @@
  */
 
 import { useTranslation } from "react-i18next";
+
 import { commonProjects } from "../../data/commonProjects";
 import ProjectCard from "../../components/cards/ProjectCard/ProjectCard";
+
+import "./CommonProjects.css";
 
 function CommonProjects() {
   const { t } = useTranslation();
@@ -14,10 +17,12 @@ function CommonProjects() {
   return (
     <section className="page">
       <div className="container">
+        {/* Page Header */}
         <h1>{t("pages.commonProjects.title")}</h1>
         <p>{t("pages.commonProjects.description")}</p>
 
-        <div className="members-grid">
+        {/* Common Projects Grid */}
+        <div className="projects-grid">
           {commonProjects.map((project) => (
             <ProjectCard key={project.id} project={project} />
           ))}
